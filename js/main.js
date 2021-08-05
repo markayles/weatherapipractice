97db41935e48c909d1a7af6d1f593424a3140ad2
Mark Ayles <marka2049@gmail.com>
2021-08-05 05:46:57 -0400

    Add a variety of weather images. Create div for image to show in. Load specific image depending on the current weather.

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ function getCityWeather(city){
             var currentCondition = json.weather[0].main;
             var cityName = json.name;
             var iconCode = json.weather[0].icon;
+            var weatherID = json.weather[0].id;
     
             console.log(currentTemp);
             console.log(forecastHi);
@@ -24,8 +25,35 @@ function getCityWeather(city){
             console.log(currentCondition);
             console.log(cityName);
             console.log("Icon code: " + iconCode);
+            console.log("Weather ID: " + weatherID);
     
-            
+
+            // The weather ID returns a 3 digit number signifying the type of weather
+            // Each type of weather is grouped into something like 100, 200, 300, etc.. seen here: https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+            // Based on the grouping, we decide what image to show. If there is weather ocurring that we don't specify, we show an unknown image
+            // This part could have definitely been coded better/differently, but i found it hilarious
+            var weatherImageSource;
+
+            if (weatherID >= 200 && weatherID < 600) // thunderstorm, drizzle, and rain
+            { 
+                weatherImageSource = "img/mike_rain.jpg"; 
+            }  
+            else if ((weatherID >= 700 && weatherID < 800) || (weatherID >= 801 && weatherID < 900)) //atmoshphere and clouds
+            { 
+                weatherImageSource = "img/mike_cloudy.jpg";
+            } 
+            else if (weatherID == 800) //clear
+            { 
+                weatherImageSource = "img/mike_sunny.jpg"; 
+            } 
+            else //everything else we never accounted for
+            { 
+                weatherImageSource = "img/mike_unknown.jpg"; 
+            } 
+
+            document.getElementById("weatherImage").innerHTML = `<img src=${weatherImageSource}>`;
+
+
             document.getElementById("weatherIcon").innerHTML = `<img src=http://openweathermap.org/img/wn/${iconCode}.png>`;
             document.getElementById("cityname").innerHTML = cityName;
             document.getElementById("weather").innerHTML = "Currently " + currentCondition;
