a25b0f920e03780bcfe815a0e7acbdaf5ebce714
Mark Ayles <marka2049@gmail.com>
2021-08-05 05:21:02 -0400

    Add weather icon div. Move API request to its own function. Call same function with a default city on page load. Add icon loading based on weather type (code is from API response for specific image)

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-var ajax = new XMLHttpRequest();
-ajax.onload = functionName;
-ajax.onerror = errorFunctionName;
-ajax.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1e26c4c0e6e44adb84fd29c67cfc558f&units=imperial`, true);
-ajax.send();
-function functionName(city) {
-    console.log(this);
-    if (this.status == 200) {
-        var json = JSON.parse(this.responseText);
-        var currentTemp = json.main.temp;
-        var forecastHi = json.main.temp_max;
-        var forecastLo = json.main.temp_min;
-        var currentCondition = json.weather[0].main;
-        var cityName = json.name;
+let defaultCity = "Tewksbury";
 
-        console.log(currentTemp)
-        console.log(forecastHi)
-        console.log(forecastLo)
-        console.log(currentCondition)
-        console.log(cityName)
+function getCityWeather(city){
+    var ajax = new XMLHttpRequest();
+    ajax.onload = functionName;
+    ajax.onerror = errorFunctionName;
+    ajax.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1e26c4c0e6e44adb84fd29c67cfc558f&units=imperial`, true);
+    ajax.send();
+    function functionName(city) {
+        console.log(this);
+        if (this.status == 200) {
+            var json = JSON.parse(this.responseText);
 
-        
-        document.getElementById("cityname").innerHTML = cityName;
-        document.getElementById("weather").innerHTML = "Currently " + currentCondition;
-        document.getElementById("hi").innerHTML = "High: " + forecastHi + "&deg;";
-        document.getElementById("current").innerHTML = "Current Temp: " + currentTemp + "&deg;";
-        document.getElementById("lo").innerHTML = "Low: " + forecastLo + "&deg;";
-    } else {
-        // handle more HTTP response codes here;
+            var currentTemp = json.main.temp;
+            var forecastHi = json.main.temp_max;
+            var forecastLo = json.main.temp_min;
+            var currentCondition = json.weather[0].main;
+            var cityName = json.name;
+            var iconCode = json.weather[0].icon;
+    
+            console.log(currentTemp);
+            console.log(forecastHi);
+            console.log(forecastLo);
+            console.log(currentCondition);
+            console.log(cityName);
+            console.log("Icon code: " + iconCode);
+    
+            
+            document.getElementById("weatherIcon").innerHTML = `<img src=http://openweathermap.org/img/wn/${iconCode}.png>`;
+            document.getElementById("cityname").innerHTML = cityName;
+            document.getElementById("weather").innerHTML = "Currently " + currentCondition;
+            document.getElementById("hi").innerHTML = "High: " + forecastHi + "&deg;";
+            document.getElementById("current").innerHTML = "Current Temp: " + currentTemp + "&deg;";
+            document.getElementById("lo").innerHTML = "Low: " + forecastLo + "&deg;";
+        } else {
+            // handle more HTTP response codes here;
+        }
     }
 }
 
@@ -52,3 +60,6 @@ form.addEventListener('submit', (event) => {
 //    else if (forecast == 'Clouds'){bg_img = 'clouds'} 
 //    else {bg_img = 'default-img'};
 //}
+
+// This will get called when the script is loaded just so we have some data shown instead of blank stuff
+getCityWeather(defaultCity);
